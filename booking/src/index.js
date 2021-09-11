789aba3994364863e5ed51fbd5755268fe814b72
alisa.hryhor12 <alisa.hryhor@forforce.com>
2021-09-11 16:19:05 +0300

    settings

---
+++ booking/src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDom from "react-dom";
+import MainPage from "./pages/main";
+
+function App() {
+    return (
+        <div>
+            <MainPage />
+        </div>
+    )
+}
+
+
+ReactDom.render(
+    <React.StrictMode><App /></React.StrictMode>,
+    document.getElementById("root")
+);
---
+++ booking/src/pages/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/Header";
+import Maincontent from "../components/Maincontent";
+
+export default function MainPage() {
+    return <>
+        <Header />
+        <Maincontent />
+    </>
+}
